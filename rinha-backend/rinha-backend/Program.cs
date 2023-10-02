fcda93b810b0fba53a4727b9d3d624b60e1c67e5
giovanni massaro <giovanni.massaro>
2023-10-01 21:43:33 -0300

    add try catch migration

--- rinha-backend/rinha-backend/Program.cs
+++ rinha-backend/rinha-backend/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddDbContext<RinhaContext>(options => options.UseNpgsql("Host=localhost;Database=root;Username=postgres;Password=2309"));
+builder.Services.AddDbContext<RinhaContext>(options => options.UseNpgsql("Host=db;Database=root;Username=postgres;Password=2309"));
 
 builder.Services.AddTransient<IPessoaRepository, PessoaRepository>();
 
@@ -17,8 +17,15 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<RinhaContext>();
-    db.Database.Migrate();
+    try
+    {
+        var db = scope.ServiceProvider.GetRequiredService<RinhaContext>();
+        db.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+
+    }
 }
 
 app.UseAuthorization();
